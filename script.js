5e018f2d1d300d1655fde126a54fa9d49bce34fe
CAUÃ DA SILVA <23111778@CFP127ALUNO.LOCAL>
2024-04-30 12:27:13 -0300

    api pronta

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-const chatMessages = document.getElementById('chat-messages');
-const userInput = document.getElementById('user-input');
-const sendBtn = document.getElementById('send-btn');
+const consultarGemini = (question) => {
+    const keyGoogle = 'AIzaSyBZaybh57iVi23jcLvzuIrabNG4f3td60A';
+    const url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=' + keyGoogle;
 
-sendBtn.addEventListener('click', async () => {
-    const userMessage = userInput.value.trim();
-    if (userMessage !== '') {
-        appendMessage('user', userMessage);
-        const aiResponse = await getAIResponse(userMessage);
-        appendMessage('ai', aiResponse);
-        userInput.value = '';
-    }
-});
+    const requestData = {
+        contents: [
+            {
+                parts: [
+                    {
+                        text: `${question}`
+                    }
+                ]
+            }
+        ]
+    };
 
-function appendMessage(sender, message) {
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestData)
+    };
+
+    fetch(url, requestOptions)
+    .then(response => response.json())
+    .then(data => {
+        const respostaTextIa = data.candidates[0].content.parts[0].text;
+        appendMessage('AI', respostaTextIa); // Adicionando a resposta como mensagem
+    })
+    .catch(error => console.error('Error: ', error ));
+}
+
+const appendMessage = (sender, message) => {
+    const chatMessages = document.getElementById('chat-messages');
     const messageDiv = document.createElement('div');
-    messageDiv.classList.add('flex', 'mb-2', 'justify-end', 'items-end');
+    messageDiv.classList.add('flex', 'mb-2', 'items-end');
 
     const messageContent = document.createElement('div');
     messageContent.classList.add('max-w-md', 'px-4', 'py-2', 'rounded-lg', 'shadow', 'bg-blue-100');
@@ -26,9 +46,14 @@ function appendMessage(sender, message) {
     messageContent.textContent = message;
     messageDiv.appendChild(messageContent);
     chatMessages.appendChild(messageDiv);
+
+    // Role automaticamente para baixo
+    chatMessages.scrollTop = chatMessages.scrollHeight;
 }
 
-async function getAIResponse(message) {
-  
-    return "Resposta da OpenAI: " + message.toUpperCase();
-}
+const sendBtn = document.getElementById('send-btn');
+sendBtn.addEventListener('click', () => {
+    const question = document.getElementById('user-input').value;
+    appendMessage('user', question); // Adicionando a mensagem do usuário
+    consultarGemini(question);
+});
