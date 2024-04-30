5f05f50fdaea82ae25cd7e5e3af7235ff4ac798e
CAUÃ DA SILVA <23111778@CFP127ALUNO.LOCAL>
2024-04-30 09:07:43 -0300

    login pronto

--- login.js
+++ login.js
@@ -1,17 +1,47 @@
-'use strict'
+'use strict';
 
-const form = document.getElementById('form').addEventListener('submit', function(event) {
-    event.preventDefault()
+const form = document.getElementById('form');
 
-    const email = document.getElementById('email').value
-    const senha = document.getElementById('senha').value
+form.addEventListener('submit', function(event) {
+    event.preventDefault(); 
 
-    if (email === 'adm@gmail.com' && senha === 'Adm123@') {
-        window.location.href = './index.html'
-    } else {
-        alert('Por favor, coloque as informações corretas!!')
+    validarLogin();
+});
+
+async function validarLogin(){
+    const email = document.getElementById('email').value;
+    const senha = document.getElementById('senha').value;
+
+    console.log(email);
+
+    if(email === '' || senha === ''){
+        alert('Preencha todos os campos');
+        return false;
     }
-})
 
+    try{
+        const users = await fetch('https://back-login.vercel.app/usuarios');
 
+        const listUsers = await users.json();
 
+        let validaUsuario = false;
+
+        listUsers.forEach((user)=>{
+            if(email === user.email && senha === user.senha){
+                alert('Usuário encontrado com sucesso!');
+                localStorage.setItem("id", user.id);
+                localStorage.setItem("nome", user.email);
+                window.location.href = './index.html';
+                validaUsuario = true;
+            }
+        });
+
+        if(!validaUsuario){
+            alert('Usuário não encontrado.');
+        }
+
+    } catch(error){
+        alert('Erro ao acessar a API');
+        console.error(error);
+    }
+}
